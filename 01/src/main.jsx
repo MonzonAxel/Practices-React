0c6e9d5af0798386d8c316eccf4f7d7bb574e52e
MonzonAxel <axel.monzon15@hotmail.com>
2024-09-12 18:50:31 -0300

    going to back change previous

--- 01/src/main.jsx
+++ 01/src/main.jsx
@@ -2,18 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Form } from './Form.jsx'
 import { Users } from './Users.jsx'
-import { Tareas } from './Tareas.jsx'
-import { Footer } from './Footer.jsx'
-import { Header } from './Header.jsx'
 import ButtonCounter from './ButtonCounter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header></Header>
     <ButtonCounter />
     <Form />
     <Users/>
-    <Tareas/>
-    <Footer></Footer>
   </StrictMode>,
 )
