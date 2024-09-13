46e985fd9b7bbd6215ebfdcfbb88d06ddfbd7355
MonzonAxel <axel.monzon15@hotmail.com>
2024-09-13 00:01:43 -0300

    add navbar

--- 01/src/main.jsx
+++ 01/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Form } from './Form.jsx'
 import { Users } from './Users.jsx'
+import { Navbar } from './Navbar.jsx'
 import ButtonCounter from './ButtonCounter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar/>
     <ButtonCounter />
     <Form />
     <Users/>
