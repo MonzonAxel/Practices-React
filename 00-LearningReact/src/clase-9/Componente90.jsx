ead3952e91558fa18eda5ed49e8c6afcea97be53
MonzonAxel <axel.monzon15@hotmail.com>
2024-10-07 22:23:30 -0300

    mantenemos las buenas practicas en el useReducer

--- 00-LearningReact/src/clase-9/Componente90.jsx
+++ 00-LearningReact/src/clase-9/Componente90.jsx
@@ -1,70 +1,83 @@
 import { useReducer } from "react";
 
-const currentTime = new Date () 
-const currentMonth = currentTime.getMonth()
-const currentYear = currentTime.getFullYear()
+const currentTime = new Date();
+const currentMonth = currentTime.getMonth();
+const currentYear = currentTime.getFullYear();
 
-const months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio",
-    "Julio","Agosto","Septiembre","Noviembre","Diciembre"]
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
 
-const initialValue= {month:currentMonth, year:currentYear}
+const initialValue = { month: currentMonth, year: currentYear };
 
 const types = {
-    incrementMonth: "incrementMonth",
-    decrementMonth: "decrementMonth",
-    incrementYear: "incrementYear",
-    decrementYear: "decrementYear"
-}
+  incrementMonth: "incrementMonth",
+  decrementMonth: "decrementMonth",
+  incrementYear: "incrementYear",
+  decrementYear: "decrementYear",
+};
 
-const reducer = (state,action) => {
-    switch(action.type){
-        case types.incrementMonth:
-            // if(state.month == 11) {
-            //     state.month = 0
-            //     return {month:state.month, year:state.year}
-            // }
-            return {month:state.month+1, year:state.year};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.incrementMonth:
+      return { ...state, month: state.month === 11 ? 0 : state.month + 1 };
 
-        case types.decrementMonth:
-            return {month:state.month-1, year:state.year};
+    case types.decrementMonth:
+      return {...state,month: state.month === 0 ? 11 : state.month - 1,};
 
-        case types.incrementYear:
-            return {month:state.month, year:state.year+1};
+    case types.incrementYear:
+      return {...state, year: state.year + 1,};
 
-        case types.decrementYear:
-            return {month:state.month, year:state.year-1};
-    }
-    return state;
-}
-
-
+    case types.decrementYear:
+      return {...state, year: state.year - 1,};
+      
+    default:
+      return state;
+  }
+};
 
 export const Component90 = () => {
-    const [state,dispatch] = useReducer(reducer,initialValue)
+  const [state, dispatch] = useReducer(reducer, initialValue);
 
   return (
     <>
       <h3>
         {months[state.month]} {state.year}
       </h3>
-      <div >
+      <div>
         Mes
-        <button style={{marginInline:"1rem"}}
-        onClick={() => dispatch({type:types.incrementMonth})}
-        >+</button>
         <button
-        onClick={() => dispatch({type:types.decrementMonth})}
-        >-</button>
+          style={{ marginInline: "1rem" }}
+          onClick={() => dispatch({ type: types.incrementMonth })}
+        >
+          +
+        </button>
+        <button onClick={() => dispatch({ type: types.decrementMonth })}>
+          -
+        </button>
       </div>
       <div>
         Año
-        <button style={{marginInline:"1rem"}}
-        onClick={() => dispatch({type:types.incrementYear})}
-        >+</button>
         <button
-         onClick={() => dispatch({type:types.decrementYear})}
-         >-
-         </button>
+          style={{ marginInline: "1rem" }}
+          onClick={() => dispatch({ type: types.incrementYear })}
+        >
+          +
+        </button>
+        <button onClick={() => dispatch({ type: types.decrementYear })}>
+          -
+        </button>
       </div>
     </>
   );
