9226298fc8dd32978b0d3d8947ecb40a2408a0d5
Axel Monzon <monzon.ax@gmail.com>
2024-10-08 12:03:10 -0300

    added empty lesson 10

---
+++ 00-LearningReact/src/clase10/Clase10.jsx
@@ -0,0 +1,11 @@
+import { Component100 } from './Component100'
+
+export const Clase10 = () => {
+  return (
+    <div>
+        <h2>Clase 10</h2>
+        <Component100></Component100>      
+    </div>
+    
+  )
+}
---
+++ 00-LearningReact/src/clase10/Component100.jsx
@@ -0,0 +1,7 @@
+
+
+export const Component100 = () => {
+  return (
+    <div>Component100 Actividad 1</div>
+  )
+}
